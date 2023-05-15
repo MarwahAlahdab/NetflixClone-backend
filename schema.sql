c58e166ced21e396fecd46c47f5cbcf5ff87c35d
marwasyr <marwaalahdab14@gmail.com>
2023-05-15 19:47:36 +0300

    database updated

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE movie if EXISTS
-CREATE TABLE IF NOT EXISTS movie (
-    id SERIAL PRIMARY KEY ,
+CREATE TABLE movie (
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    release_date INTEGER NOT NULL,
-    overview VARCHAR(255) NOT NULL);
+    release_date DATE NOT NULL,
+    overview VARCHAR(255) NOT NULL
+);
